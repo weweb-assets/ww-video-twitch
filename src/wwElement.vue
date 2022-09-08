bda1f5b5d3469dfb7082a7a58e73d213dff43761
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2022-09-08 10:57:12 +0200

    First commit

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="ww-video"></div>
+    <div class="ww-video-dailymotion"></div>
 </template>
 
 <script>
@@ -15,64 +15,20 @@ export default {
         return {};
     },
     computed: {},
-    watch: {
-        'content.autoplay'(newAuto, oldAuto) {
-            if (this.content.autoplay) {
-                this.$emit('update:content:effect', {
-                    muted: true,
-                });
-            }
-            if (newAuto !== oldAuto && newAuto) {
-                this.updateweWeWebVideo();
-            }
-        },
-        'content.loop'(newLoop, oldLoop) {
-            if (newLoop !== oldLoop && newLoop) {
-                this.updateweWeWebVideo();
-            }
-        },
-    },
-    unmounted() {
-        if (this.isEventListener) {
-            const videoEl = document.querySelector('.ww-video-element');
-            videoEl.removeEventListener('click', this.handleVideoClick);
-            this.isEventListener = false;
-        }
-    },
-    methods: {
-        getInfoFromUrl(url) {
-            if (!this.content.url) return {};
-            return {
-                id: url.split('m/')[1].split('?')[0],
-                provider: 'vimeo',
-            };
-        },
-    },
+    watch: {},
+    unmounted() {},
+    methods: {},
 };
 </script>
 
 <style lang="scss" scoped>
-.ww-video {
+.ww-video-dailymotion {
     position: relative;
     width: 100%;
     height: 100%;
-    .ww-video-container {
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        top: 0;
-        left: 0;
 
-        .ww-video-element {
-            position: relative;
-            width: 100%;
-            height: 100%;
-            overflow: hidden;
-
-            &.ww-editing {
-                pointer-events: none;
-            }
-        }
+    &.ww-editing {
+        pointer-events: none;
     }
 }
 </style>
