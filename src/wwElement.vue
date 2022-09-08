71855499cbf0fd1a38e6eddcb1ee334fa39e9662
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2022-09-08 23:02:41 +0200

    Main commit

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,34 +1,48 @@
 <template>
-    <div class="ww-video-dailymotion"></div>
+    <div class="ww-video-twitch" :class="{ editing: isEditing }"></div>
 </template>
 
 <script>
+import { nextTick } from 'vue';
+
 export default {
     props: {
         content: { type: Object, required: true },
+        uid: { type: String, required: true },
         /* wwEditor:start */
         wwEditorState: { type: Object, required: true },
         /* wwEditor:end */
     },
-    emits: ['update:content:effect'],
-    data() {
-        return {};
+    computed: {
+        isEditing() {
+            /* wwEditor:start */
+            return this.wwEditorState.editMode === wwLib.wwEditorHelper.EDIT_MODES.EDITION;
+            /* wwEditor:end */
+            // eslint-disable-next-line no-unreachable
+            return false;
+        },
     },
-    computed: {},
-    watch: {},
-    unmounted() {},
-    methods: {},
 };
 </script>
 
 <style lang="scss" scoped>
-.ww-video-dailymotion {
+.ww-video-twitch {
     position: relative;
-    width: 100%;
-    height: 100%;
+    overflow: hidden;
+    aspect-ratio: 16 / 9;
 
-    &.ww-editing {
+    &.editing {
         pointer-events: none;
+    }
+
+    &__player {
+        position: absolute;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        width: 100%;
+        height: 100%;
     }
 }
 </style>
--- ww-config.js
+++ ww-config.js
@@ -4,10 +4,130 @@ export default {
     },
     editor: {
         label: {
-            fr: 'Vidéo - Dailymotion',
-            en: 'Vidéo - Dailymotion',
+            fr: 'Vidéo - Twitch',
+            en: 'Vidéo - Twitch',
         },
         icon: 'play',
     },
-    properties: {},
+    triggerEvents: [
+        { name: 'play', label: { en: 'On play' }, event: { value: '' }, default: true },
+        { name: 'pause', label: { en: 'On pause' }, event: { value: '' } },
+        { name: 'end', label: { en: 'On end' }, event: { value: '' } },
+    ],
+    properties: {
+        url: {
+            path: 'url',
+            label: { en: 'Video url', fr: 'Url de la vidéo' },
+            type: 'Text',
+            section: 'settings',
+            options: {
+                placeholder: 'Url',
+            },
+            bindable: true,
+            hidden: content => content.provider === 'weweb',
+            defaultValue: 'https://www.dailymotion.com/video/x84sh87',
+        },
+        videoStartTime: {
+            label: {
+                en: 'Start time (s)',
+            },
+            type: 'Number',
+            options: (_, sidepanelContent) => {
+                console.log(sidepanelContent.videoDuration);
+                return { min: 0, max: sidepanelContent.videoDuration };
+            },
+            section: 'settings',
+            bindable: true,
+            defaultValue: 0,
+        },
+        videoDuration: {
+            editorOnly: true,
+            defaultValue: 0,
+            hidden: true,
+        },
+        autoplay: {
+            label: { en: 'Autoplay', fr: 'Lecture automatique' },
+            type: 'TextRadioGroup',
+            section: 'settings',
+            options: {
+                choices: [
+                    {
+                        default: true,
+                        value: false,
+                        title: { en: 'Start', fr: 'Début' },
+                        icon: 'none',
+                    },
+                    {
+                        value: true,
+                        title: { en: 'Center', fr: 'Milieu' },
+                        icon: 'tick',
+                    },
+                ],
+            },
+            defaultValue: false,
+        },
+        muted: {
+            label: { en: 'Muted', fr: 'Muet' },
+            type: 'TextRadioGroup',
+            section: 'settings',
+            options: {
+                choices: [
+                    {
+                        default: true,
+                        value: false,
+                        title: { en: 'Start', fr: 'Début' },
+                        icon: 'none',
+                    },
+                    {
+                        value: true,
+                        title: { en: 'Center', fr: 'Milieu' },
+                        icon: 'tick',
+                    },
+                ],
+            },
+            defaultValue: false,
+        },
+        loop: {
+            label: { en: 'Loop', fr: 'Lecture en boucle' },
+            type: 'TextRadioGroup',
+            section: 'settings',
+            options: {
+                choices: [
+                    {
+                        default: true,
+                        value: false,
+                        title: { en: 'Start', fr: 'Début' },
+                        icon: 'none',
+                    },
+                    {
+                        value: true,
+                        title: { en: 'Center', fr: 'Milieu' },
+                        icon: 'tick',
+                    },
+                ],
+            },
+            defaultValue: false,
+        },
+        controls: {
+            label: { en: 'Controls', fr: 'Contrôles' },
+            type: 'TextRadioGroup',
+            section: 'settings',
+            options: {
+                choices: [
+                    {
+                        value: false,
+                        title: { en: 'Start', fr: 'Début' },
+                        icon: 'none',
+                    },
+                    {
+                        default: true,
+                        value: true,
+                        title: { en: 'Center', fr: 'Milieu' },
+                        icon: 'tick',
+                    },
+                ],
+            },
+            defaultValue: true,
+        },
+    },
 };
